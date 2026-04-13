d73fddec939a3ce149379a0ed442a416579a210f
tuliarnav0-source <tuliarnav0@gmail.com>
2026-04-13 13:28:25 +0530

    Create deletionAtindex circular.c

---
+++ deletionAtindex circular.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    int data;
+    struct Node*next;
+};
+void linkedListTraversal(struct Node*head){
+    struct Node*ptr=head;
+    printf("Element:%d\n",ptr->data);
+    ptr=ptr->next;
+    while(ptr!=head){
+        printf("element:%d\n",ptr->data);
+        ptr=ptr->next;
+    }
+}
+struct Node*deleteAtIndex(struct Node*head,int index){
+   struct Node*p=head;
+   struct Node*q=p->next;
+   for(int i=0;i<index-1;i++){
+       p=p->next;
+       q=q->next;
+   }
+   p->next=q->next;
+   free(q);
+   return head;
+}
+
+int main(){
+    struct Node*head;
+    struct Node*second;
+    struct Node*third;
+    struct Node*fourth;
+    
+    head=(struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+    fourth = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 7;
+    head->next = second;
+
+    second->data = 11;
+    second->next = third;
+
+    third->data = 41;
+    third->next = fourth;
+
+    fourth->data = 68;
+    fourth->next = head;
+
+    printf("CircularLinkedlist before deletion:\n");
+    linkedListTraversal(head);
+    head=deleteAtIndex(head,2);
+    printf("Circularlinkedlist after deletion:\n");
+    linkedListTraversal(head);
+    return 0;
+} 
