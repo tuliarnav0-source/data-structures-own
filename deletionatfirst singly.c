cb96afd718cb92d1dd3983a04e5503a12a905d3d
tuliarnav0-source <tuliarnav0@gmail.com>
2026-04-13 13:23:55 +0530

    Create deletionatfirst singly.c

---
+++ deletionatfirst singly.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    int data;
+    struct Node*next;
+};
+void linkedListTraversal(struct Node*ptr){
+    while(ptr!=NULL){
+        printf("element:%d\n",ptr->data);
+        ptr=ptr->next;
+    }
+}
+struct Node*deleteFirst(struct Node*head){
+    struct Node*ptr=head;
+    head=head->next;
+    free(ptr);
+    return head;
+}
+int main(){
+    struct Node*head;
+    struct Node*second;
+    struct Node*third;
+    struct Node*fourth;
+    
+    head=(struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+    fourth = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 7;
+    head->next = second;
+
+    second->data = 11;
+    second->next = third;
+
+    third->data = 41;
+    third->next = fourth;
+
+    fourth->data = 68;
+    fourth->next = NULL;
+
+    printf("Linkedlist before deletion:\n");
+    linkedListTraversal(head);
+    head=deleteFirst(head);
+    printf("linkedlist after deletion:\n");
+    linkedListTraversal(head);
+    return 0;
+}
