20b44e1bf308af481872a29ab5e378eeef2afe66
tuliarnav0-source <tuliarnav0@gmail.com>
2026-04-13 13:23:25 +0530

    Create insertionatend singly.c

---
+++ insertionatend singly.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    int data;
+    struct Node*next;
+};
+
+void linkedListTraversal(struct Node*ptr){
+    while(ptr!=NULL){
+        printf("element:%d\n",ptr->data);
+        ptr=ptr->next;
+    }
+}
+struct Node*insertAtEnd(struct Node*head,int data){
+    struct Node*ptr=(struct Node*)malloc(sizeof(struct Node));
+    struct Node*p=head;
+    ptr->data=data;
+    while(p->next!=NULL){
+        p=p->next;
+    }
+    p->next=ptr;
+    ptr->next=NULL;
+    return head;
+}
+
+int main(){
+    struct Node* head;
+    struct Node* second;
+    struct Node* third;
+    struct Node* fourth;
+
+    head = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+    fourth = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 7;
+    head->next = second;
+
+    second->data = 11;
+    second->next = third;
+
+    third->data = 41;
+    third->next = fourth;
+
+    fourth->data = 68;
+    fourth->next = NULL;
+
+    printf("Linkedlist before insertion:\n");
+    linkedListTraversal(head);
+    head=insertAtEnd(head,88);
+    printf("linkedlist after insertion:\n");
+    linkedListTraversal(head);
+    return 0;
+}
