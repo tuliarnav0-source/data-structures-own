1a2d08842b2cdd9c4208b8f631eaf94a8bd30d57
tuliarnav0-source <tuliarnav0@gmail.com>
2026-04-25 18:28:23 +0530

    Create postOrdertraversal.c

---
+++ postOrdertraversal.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<malloc.h>
+struct Node{
+    int data;
+    struct Node*left;
+    struct Node*right;
+};
+struct Node*createnode(int data){
+    struct Node*n;
+    n=(struct Node*)malloc(sizeof(struct Node));
+    n->data=data;
+    n->left=NULL;
+    n->right=NULL;
+    return n;
+}
+void postOrder(struct Node*root){
+    if(root!=NULL){
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d",root->data);
+    }
+}
+int main(){
+    struct Node*p=createnode(1);
+    struct Node*p1=createnode(2);
+    struct Node*p2=createnode(3);
+    struct Node*p3=createnode(4);
+    struct Node*p4=createnode(5);
+    
+    p->left=p1;
+    p->right=p2;
+    p1->left=p3;
+    p1->right=p4;
+    
+    postOrder(p);
+    return 0;
+}
